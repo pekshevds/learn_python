4dfdab40d2f77001f3a7e43015f7180635777b1f
pekshevds <pekshev.ds@gmail.com>
2022-09-05 15:35:27 +0300

    correct mistakes

---
+++ telegram-bot/bot.py
@@ -0,0 +1,48 @@
+import logging
+import requests
+from telegram.ext import Updater, CommandHandler, \
+                            MessageHandler, Filters
+from config import telegram_bot_token, weather_token
+
+
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
+
+
+
+def greet_user(update, context):
+    update.message.reply_text("Привет, введи имя города")
+
+def show_weather(update, context):
+    """ get weather data from openweather and send current weather to telegram """
+    
+    city_name = update.message.text.strip()    
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={weather_token}&lang=ru&units=metric")
+    
+    if response.status_code == 404:
+        update.message.reply_text(f"Город с именем {city_name} не найден")
+        return
+    
+    json = response.json()
+    answer = f"""
+    Погода в {json['name']}:
+    {json['weather'][0]['description']}, температура {json['main']['temp']} (по ощущениям {json['main']['feels_like']})
+    """
+    update.message.reply_text(answer)
+
+def main():
+    """ my first telegram bot """
+    mybot = Updater(telegram_bot_token)
+
+    dp = mybot.dispatcher
+    dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.text, show_weather))
+
+    logging.info("bot started")
+
+    mybot.start_polling()
+    mybot.idle()
+
+
+if __name__ == "__main__":
+    main()
