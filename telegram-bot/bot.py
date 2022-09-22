524e0d28196f6a4ebc3d00c825eefa30e6c47a15
pekshevds <pekshev.ds@gmail.com>
2022-09-22 12:57:28 +0300

    keyboard

--- telegram-bot/bot.py
+++ telegram-bot/bot.py
@@ -1,9 +1,9 @@
 import logging
 import requests
+from telegram import ReplyKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, \
                             MessageHandler, Filters
 import config
-
 
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -11,25 +11,48 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-
 def greet_user(update, context):
-    update.message.reply_text("Привет, введи имя города")
+
+    my_keyboard = ReplyKeyboardMarkup([['Погода в белгороде']])
+
+    update.message.reply_text(
+        "Привет, введи имя города",
+        reply_markup=my_keyboard
+        )
+
+def get_city_weather(city_name):
+    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={config.WEATHER_TOKEN}&lang=ru&units=metric")
+    if response.ok:
+        return response.json()
+    return False
+
+def show_weather_in_belgorod(update, context):
+    """ get weather data from openweather and send current weather to telegram """
+                
+    city_name = 'belgorod'
+    city_weather = get_city_weather(city_name=city_name)
+    if city_weather:
+        answer = f"""
+        Погода в {city_weather['name']}:
+        {city_weather['weather'][0]['description']}, температура {city_weather['main']['temp']} (по ощущениям {city_weather['main']['feels_like']})
+        """
+    else:
+        answer = f"Город с именем {city_name} не найден"    
+    update.message.reply_text(answer)
 
 def show_weather(update, context):
     """ get weather data from openweather and send current weather to telegram """
     
     city_name = update.message.text.strip()    
-    response = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={config.WEATHER_TOKEN}&lang=ru&units=metric")
-    
-    if response.status_code == 404:
-        update.message.reply_text(f"Город с именем {city_name} не найден")
-        return
-    
-    json = response.json()
-    answer = f"""
-    Погода в {json['name']}:
-    {json['weather'][0]['description']}, температура {json['main']['temp']} (по ощущениям {json['main']['feels_like']})
-    """
+        
+    city_weather = get_city_weather(city_name=city_name)
+    if city_weather:
+        answer = f"""
+        Погода в {city_weather['name']}:
+        {city_weather['weather'][0]['description']}, температура {city_weather['main']['temp']} (по ощущениям {city_weather['main']['feels_like']})
+        """
+    else:
+        answer = f"Город с именем {city_name} не найден"    
     update.message.reply_text(answer)
 
 def main():
@@ -38,6 +61,7 @@ def main():
 
     dp = mybot.dispatcher
     dp.add_handler(CommandHandler("start", greet_user))
+    dp.add_handler(MessageHandler(Filters.regex('^(Погода в белгороде)$'), show_weather_in_belgorod))
     dp.add_handler(MessageHandler(Filters.text, show_weather))
 
     mybot.start_polling()
